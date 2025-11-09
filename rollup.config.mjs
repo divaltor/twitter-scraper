49882e42e11b542b52dceb7f87fe10e0afe5c611
Vlad Vladov <divaltor@pm.me>
2025-11-09 15:12:33 +0200

    Get rid of PLATFORM_NODE

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,95 +1,70 @@
-import dts from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
+import dts from "rollup-plugin-dts";
 
 export default [
-  {
-    input: 'src/_module.ts',
-    plugins: [
-      esbuild({
-        define: {
-          PLATFORM_NODE: 'false',
-          PLATFORM_NODE_JEST: 'false',
-        },
-      }),
-    ],
-    output: [
-      {
-        file: 'dist/default/cjs/index.js',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: 'dist/default/esm/index.mjs',
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: 'src/_module.ts',
-    plugins: [
-      esbuild({
-        define: {
-          PLATFORM_NODE: 'true',
-          PLATFORM_NODE_JEST: 'false',
-        },
-      }),
-    ],
-    output: [
-      {
-        file: 'dist/node/cjs/index.cjs',
-        format: 'cjs',
-        sourcemap: true,
-        inlineDynamicImports: true,
-      },
-      {
-        file: 'dist/node/esm/index.mjs',
-        format: 'es',
-        sourcemap: true,
-        inlineDynamicImports: true,
-      },
-    ],
-  },
-  {
-    input: 'src/_module.ts',
-    plugins: [dts()],
-    output: {
-      file: 'dist/types/index.d.ts',
-      format: 'es',
-    },
-  },
-  // CycleTLS build (Node-only)
-  {
-    input: 'src/_cycletls.ts',
-    external: ['cycletls'],
-    plugins: [
-      esbuild({
-        define: {
-          PLATFORM_NODE: 'true',
-          PLATFORM_NODE_JEST: 'false',
-        },
-      }),
-    ],
-    output: [
-      {
-        file: 'dist/cycletls/cjs/index.cjs',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: 'dist/cycletls/esm/index.mjs',
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: 'src/_cycletls.ts',
-    external: ['cycletls'],
-    plugins: [dts()],
-    output: {
-      file: 'dist/cycletls/index.d.ts',
-      format: 'es',
-    },
-  },
+	{
+		input: "src/_module.ts",
+		output: [
+			{
+				file: "dist/default/cjs/index.js",
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: "dist/default/esm/index.mjs",
+				format: "es",
+				sourcemap: true,
+			},
+		],
+	},
+	{
+		input: "src/_module.ts",
+		output: [
+			{
+				file: "dist/node/cjs/index.cjs",
+				format: "cjs",
+				sourcemap: true,
+				inlineDynamicImports: true,
+			},
+			{
+				file: "dist/node/esm/index.mjs",
+				format: "es",
+				sourcemap: true,
+				inlineDynamicImports: true,
+			},
+		],
+	},
+	{
+		input: "src/_module.ts",
+		plugins: [dts()],
+		output: {
+			file: "dist/types/index.d.ts",
+			format: "es",
+		},
+	},
+	// CycleTLS build (Node-only)
+	{
+		input: "src/_cycletls.ts",
+		external: ["cycletls"],
+		output: [
+			{
+				file: "dist/cycletls/cjs/index.cjs",
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: "dist/cycletls/esm/index.mjs",
+				format: "es",
+				sourcemap: true,
+			},
+		],
+	},
+	{
+		input: "src/_cycletls.ts",
+		external: ["cycletls"],
+		plugins: [dts()],
+		output: {
+			file: "dist/cycletls/index.d.ts",
+			format: "es",
+		},
+	},
 ];
